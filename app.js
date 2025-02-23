834b24e2218f76e42e6abbe418935fa643ffd4a0
Bryan Sigala <bryansigala@hotmail.com>
2025-02-22 23:38:16 -0600

    Add: add actualizarLista function to app.js file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
 const entrada_amigo = document.getElementById('amigo');  
 const agregarBtn = document.getElementsByClassName('button-add');
 const sortaerBtn = document.getElementsByClassName('button-draw'); 
+const lista = document.getElementById('listaAmigos'); 
+const resultado = document.getElementById('resultado');
 
 let listaAmigos = []; 
 
@@ -11,6 +13,13 @@ let listaAmigos = [];
 
 function agregarAmigo () {
     listaAmigos.push(entrada_amigo.value); 
+    actualizarLista(); 
     entrada_amigo.value = ''; 
     console.log(listaAmigos);
+} 
+
+function actualizarLista () {
+    const nuevoAmigo = document.createElement('li'); 
+    nuevoAmigo.textContent = entrada_amigo.value;
+    lista.appendChild(nuevoAmigo); 
 }
