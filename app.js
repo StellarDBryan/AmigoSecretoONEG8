a98e3f1e791fe43fc0bfa34a5d959519e61a4dd2
Bryan Sigala <bryansigala@hotmail.com>
2025-02-22 23:23:40 -0600

    Add: add agregarAmigo function in app.js file

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+
+const entrada_amigo = document.getElementById('amigo');  
+const agregarBtn = document.getElementsByClassName('button-add');
+const sortaerBtn = document.getElementsByClassName('button-draw'); 
+
+let listaAmigos = []; 
+
+// agregarBtn.addEventListener('click', agregarAmigo()); 
+
+function agregarAmigo () {
+    listaAmigos.push(entrada_amigo.value); 
+    entrada_amigo.value = ''; 
+    console.log(listaAmigos);
+}
